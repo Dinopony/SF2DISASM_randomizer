7256489d713bfb9f00dceaf868f82a6f63f9acff
Dinopony <dinopony.gg@gmail.com>
2020-06-07 20:56:04 +0200

    Revamped Parmecia arrival boat cutscene

--- disasm/data/maps/entries/map14/mapsetups/scripts_2.asm
+++ disasm/data/maps/entries/map14/mapsetups/scripts_2.asm
@@ -75,144 +75,18 @@ cs_58FA4:       textCursor $8D6
                 hide 132
                 hide 133
                 hide 134
-                entityActionsWait 128
-                 moveRight 2
-                endActions
-                entityActionsWait 128
-                 moveUp 1
-                endActions
-                nextText $0,128         ; "We can't go back to {N}Granseal without a ship...{W2}"
-                nextSingleText $0,128   ; "There's no Granseal on{N}Grans Island now...{N}I know that, but...{W1}"
-                csWait 60
-                nextSingleText $0,131   ; "Somebody, help!{W1}"
-                customActscriptWait 131
-                 ac_setSpeed 48,48      ;   
-                 ac_jump eas_Idle       ;   
-                ac_end
-                entityActionsWait 131
-                 moveUp 6
-                endActions
-                setActscriptWait 131,eas_Init
-                setFacing 128,DOWN
-                nod 131
-                nod 131
-                stopEntity 131
-                entityActionsWait 128
-                 moveDown 1
-                endActions
-                nextSingleText $0,128   ; "What's wrong?{W1}"
-                csWait 30
-                startEntity 131
-                nextText $0,131         ; "One of my friends has not{N}returned from North Cliff.{W2}"
-                nextSingleText $0,131   ; "He may be in trouble.{W1}"
-                nextSingleText $0,128   ; "This isn't good.{W1}"
-                setFacing 128,LEFT
-                csWait 30
-                setFacing 128,RIGHT
-                csWait 30
-                setFacing 128,UP
-                csWait 30
-                nextText $0,128         ; "{LEADER}, did you{N}hear that?{W2}"
-cs_59122:       textCursor $8E3
-                nextText $0,128         ; "Would you go to North{N}Cliff and check on his{N}friend?{D1}"
-                yesNo
-                jumpIfFlagSet $59,cs_59142 ; YES/NO prompt answer
-                nextText $0,128         ; "What did you say?{N}I didn't hear you.{W2}"
-                nextText $0,128         ; "OK, once again!{W1}"
-                jump cs_59122
-cs_59142:       textCursor $8E6
-                nextText $0,128         ; "Oh, you're so brave!{W2}"
-                nextSingleText $0,128   ; "Hey, he's going with you!{W1}"
-                entityActionsWait 128
-                 moveLeft 1
-                endActions
-                setFacing 128,RIGHT
-                textCursor $8E8
-                entityActionsWait 131
-                 moveUp 2
-                endActions
-                nextText $0,131         ; "{LEADER}, thank you!{W2}"
-                nextSingleText $0,131   ; "I'll show you to North Cliff.{N}Follow me.{W1}"
-                csWait 30
-                entityActions 131
-                 moveDown 4
-                endActions
-                entityActions ALLY_BOWIE
-                 moveDown 4
-                endActions
-                entityActionsWait ALLY_JAHA
-                 moveUp 5
-                endActions
-                setFacing ALLY_BOWIE,LEFT
-                setFacing ALLY_JAHA,RIGHT
-                setCamDest 8,10
-                nextSingleText $0,ALLY_JAHA ; "{LEADER}, wait.  Wait!{W1}"
-                csWait 30
-                nextSingleText $0,ALLY_SARAH ; "Hey, you're all wet.{W1}"
-                setPos ALLY_KAZIN,13,10,DOWN
-                setPos ALLY_CHESTER,13,9,DOWN
-                setPos ALLY_SLADE,13,8,DOWN
-                setPos ALLY_SARAH,13,7,DOWN
-                setPos ALLY_KIWI,13,6,DOWN
-                entityActions ALLY_KAZIN
-                 moveDown 4
-                 moveRight 1
-                 moveDown 1
-                endActions
-                entityActions ALLY_CHESTER
-                 moveDown 5
-                 moveRight 1
-                endActions
-                entityActions ALLY_SLADE
-                 moveDown 6
-                 moveLeft 1
-                endActions
-                entityActions ALLY_SARAH
-                 moveDown 7
-                endActions
-                entityActionsWait ALLY_KIWI
-                 moveDown 7
-                endActions
-                setFacing ALLY_SLADE,DOWN
-                setFacing ALLY_SARAH,DOWN
-                setFacing ALLY_KIWI,DOWN
-                setFacing ALLY_CHESTER,DOWN
-                setFacing ALLY_KAZIN,LEFT
-                setFacing ALLY_BOWIE,UP
-                setFacing 131,UP
-                setFacing 128,DOWN
-                nextSingleText $0,ALLY_SARAH ; "Are you leaving?{N}Say it isn't so!{W1}"
-                nextSingleText $0,ALLY_KAZIN ; "I think it's too dangerous{N}out there alone.{W1}"
-                nextText $0,131         ; "And...{W2}"
-                nextSingleText $0,131   ; "It would be safer if we came{N}along.  Let's go.{W1}"
-                csWait 60
-                setCameraEntity 65535
-                entityActions 131
-                 moveDown 7
-                endActions
-                entityActions ALLY_BOWIE
-                 moveDown 7
-                endActions
-                entityActions ALLY_SARAH
-                 moveDown 7
-                endActions
-                entityActions ALLY_CHESTER
-                 moveDown 7
-                endActions
-                entityActions ALLY_JAHA
-                 moveDown 7
-                endActions
-                entityActions ALLY_KAZIN
-                 moveDown 7
-                endActions
-                entityActions ALLY_SLADE
-                 moveDown 7
-                endActions
-                entityActionsWait ALLY_KIWI
-                 moveDown 7
-                endActions
-                setStoryFlag $8         ; Battle 8 unlocked
-                warp $48,$1,$1,$3
+                setF $1FC ; Mark battle 8 as completed
+                setF $1FE ; Mark battle 10 as completed
+                setF $200 ; Mark battle 12 as completed
+                setF $202 ; Mark battle 14 as completed
+                setF $205 ; Mark battle 17 as completed
+                setF $207 ; Mark battle 19 as completed
+                setF $209 ; Mark battle 21 as completed
+                setF $19B ; Mark battle 11 as unlocked
+                setF $2C4 ; Remove Ribble cutscenes (show NPCs)
+                setF $2C5 ; Remove Ribble cutscenes (open doors)
+                setF $2C8 ; Remove Peter cutscene triggering Volcanon mountainous path battle
+                warp $49,$3E,$31,$2
                 csc_end
 ce_59270:       mainEntity 13,11,DOWN
                 entity 12,20,UP,ALLY_JAHA,eas_Init
